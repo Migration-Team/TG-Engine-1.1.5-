5fe1a037366d37a01619d31a2b2258607ef90944
fnfpwp <117086223+pwp114514@users.noreply.github.com>
2023-09-10 14:39:23 +0800

    Update FlxNewHitbox.hx

--- source/android/FlxNewHitbox.hx
+++ source/android/FlxNewHitbox.hx
@@ -20,6 +20,8 @@ class FlxNewHitbox extends FlxSpriteGroup
 	public var buttonRight:FlxButton = new FlxButton(0, 0);
     
     public var buttonSpace:FlxButton = new FlxButton(0, 0);
+    public var buttonShift:FlxButton = new FlxButton(0, 0);
+    
     
 	/**
 	 * Create the zone.
@@ -50,6 +52,15 @@ class FlxNewHitbox extends FlxSpriteGroup
 		        add(buttonRight = createHint((FlxG.width / 2) + (FlxG.width / 4), (FlxG.height / 5) * 1, Std.int(FlxG.width / 4), Std.int(FlxG.height * 0.8), 0xFF0000));
                 add(buttonSpace = createHint(0, 0, FlxG.width, Std.int(FlxG.height / 5), 0xFFFF00));		    
 		    
+		    }
+		    else if (ClientPrefs.data.hitboxLocation == 'TopShift'){
+		        add(buttonLeft = createHint(0, (FlxG.height / 5) * 1, Std.int(FlxG.width / 4), Std.int(FlxG.height * 0.8), 0xFF00FF));
+		        add(buttonDown = createHint(FlxG.width / 4, (FlxG.height / 5) * 1, Std.int(FlxG.width / 4), Std.int(FlxG.height * 0.8), 0x00FFFF));
+		        add(buttonUp = createHint(FlxG.width / 2, (FlxG.height / 5) * 1, Std.int(FlxG.width / 4), Std.int(FlxG.height * 0.8), 0x00FF00));
+		        add(buttonRight = createHint((FlxG.width / 2) + (FlxG.width / 4), (FlxG.height / 5) * 1, Std.int(FlxG.width / 4), Std.int(FlxG.height * 0.8), 0xFF0000));
+                	add(buttonSpace = createHint((FlxG.width / 2), 0, Std.int(FlxG.width / 2), Std.int(FlxG.height / 4), 0xFFFF00));
+		        add(buttonShift = createHint(0, 0, Std.int(FlxG.width / 2), Std.int(FlxG.height / 4), 0xFF0000));
+		        
 		    }
 		    
 		    else{
